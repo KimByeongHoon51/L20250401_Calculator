f679b198e541ac04503580560a49cccf30c975c1
KimByeongHoon51 <byeoku501@naver.com>
2025-04-01 09:25:18 +0900

    Initial commit

---
+++ Calculator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a;
+	int b;
+	float c;
+	float d;
+
+	cout << "정수 입력: ";
+	cin >> a;
+	cout << "정수 입력: ";
+	cin >> b;
+	cout << a << " + " << b << " = " << a + b << endl;
+	cout << a << " - " << b << " = " << a - b << endl;
+	cout << a << " * " << b << " = " << a * b << endl;
+	cout << a << " / " << b << " = " << a / b << endl;
+	cout << endl;
+
+	cout << "실수 입력: ";
+	cin >> c;
+	cout << "실수 입력: ";
+	cin >> d;
+	cout << c << " + " << d << " = " << c + d << endl;
+	cout << c << " - " << d << " = " << c - d << endl;
+	cout << c << " * " << d << " = " << c * d << endl;
+	cout << c << " / " << d << " = " << c / d << endl;
+
+}
