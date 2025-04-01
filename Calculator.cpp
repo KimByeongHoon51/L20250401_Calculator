37beabc57c4fd8889338574644454786c7b30d07
KimByeongHoon51 <byeoku501@naver.com>
2025-04-01 10:05:53 +0900

    정답 코드 추가

--- Calculator.cpp
+++ Calculator.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
+	// int Number1 = 10;
+	// int Number2 = 20;
+
+	// cout << (Number1 + Number2) << endl; 변수마다 계산 순서가 어떤지 모르기 때문에 괄호 써놓는 습관 들이기
+	// cout << (Number1 - Number2) << endl;
+	// cout << (Number1 * Number2) << endl;
+	// cout << (Number1 / Number2) << endl;
+
+	// float Number3 = 10.5;
+	// float Number4 = 2.0;
+
+	// cout << (Number3 + Number4) << endl;
+	// cout << (Number3 - Number4) << endl;
+	// cout << (Number3 * Number4) << endl;
+	// cout << (Number3 / Number4) << endl;
+
 	int a;
 	int b;
 	float c;
@@ -29,4 +45,5 @@ int main()
 	cout << c << " * " << d << " = " << c * d << endl;
 	cout << c << " / " << d << " = " << c / d << endl;
 
+	return 0;
 }
